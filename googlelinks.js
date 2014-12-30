4c600f4a6b218ba394337d31a8d5f46945992983
ks-s-a <ksenofontovsa@yandex.ru>
2014-12-30 23:31:22 +0300

    pffff

--- googlelinks.js
+++ googlelinks.js
@@ -27,12 +27,23 @@ casper
 
 casper.then(function() {
     // aggregate results for the 'phantomjs' search
-    links = links.concat(this.evaluate(getLinks));
-    this.echo('links counter: ', links.length);
+    links = links.concat(this.evaluate(getLinks)).map(function(v) {return v.slice(7).split('&')[0];}).slice(0, 2);
+
+    this.echo('Links completed! : ', links);
+});
+
+casper.each(links, function(casper, link) {
+  //this.echo('Each link must operate!');
+
+  this.open(link, function() {
+
+    this.echo('Title is: ', this.getTitle() );
+
+  });
 });
 
 casper.run(function() {
     // echo results in some pretty fashion
-    this.echo(links.length + ' links found:');
+    //this.echo(links.length + ' links found:');
     this.echo(' - ' + links.join('\n - ')).exit();
 });
