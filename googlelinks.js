77f6eaa9758d0534c01af026ca8d171b7bd4acab
ks-s-a <ksenofontovsa@yandex.ru>
2015-01-07 16:26:45 +0300

    Page jumps complited!

--- googlelinks.js
+++ googlelinks.js
@@ -5,35 +5,14 @@ var arrow;
 var start = 0;
 var url;
 
-function getLinks() {
-    var links = document.querySelectorAll('h3.r a');
-    var response = {};
-
-    response.searchEnd = document.querySelector('#pnnext');
-
-    //searchEnd = document.querySelectorAll('#pnnext') === null;
-    //this.echo(document.querySelectorAll('#pnnext'));
-    response.data = Array.prototype.map.call(links, function(e) {
-        return e.getAttribute('href');
-    });
-
-    return response;
-}
-
-function evalAndAddLinks(ctx) {
-  var res = ctx.evaluate(getLinks);
-
-  res.data.forEach(function(v) { console.log( v.slice(7).split('&')[0] )});
-
-  for (var prop in res.searchEnd)
-    console.log('res.searchEnd is: ', res.searchEnd[prop]);
-}
+//casper.userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36');
 
 casper
-  .start('http://google.fr/', function() {
+  .start('http://google.ru/', function() {
       // search for 'casperjs' from google form
-      this.fill('form[action="/search"]', { q: 'site:take365.org' }, true);
-  });
+      this.fill('form[action="/search"]', { q: 'site:udaff.com' }, true);
+      //this.sendKeys('.gbqfb', casper.page.event.key.Enter , {keepFocus: true});  
+});
   /*.each(links, function(self, link) {
     self.thenOpen(newlink, function() {
 
@@ -49,31 +28,26 @@ casper
 casper.then(function() {
     // aggregate results for the 'phantomjs' search
 
-    //url = this.getCurrentUrl();
+    url = this.getCurrentUrl();
 
-    evalAndAddLinks(this);
+this.capture('pic.png');
+
+    checkNextPageAndClick.call(this);
 });
 
-casper.repeat(9, function() {
-  if (searchEnd)
-    this.exit();
+/*casper.repeat(1, function() {
+
+  //if (!this.getHTML('td.b', true))
+  //  return;
+
+  console.log( this.getCurrentUrl() );
+  console.log(this.getTitle());
+  console.log( this.('h3.r a') );
 
   start += 10;
 
   this.thenOpen(url + '&start=' + start, function() {
     evalAndAddLinks(this);
-  });
-});
-
-//casper.thenOpen()
-
-/*casper.eachThen(links, function(link) {
-  console.log('Each link must operate!');
-
-  this.thenOpen(link.data, function(res) {
-
-    console.log('Res is: ', res.url );
-
   });
 });*/
 
@@ -83,3 +57,36 @@ casper.run(function() {
     //this.echo(' - ' + links.join('\n - '))
     this.exit();
 });
+
+function getLinks() {
+    var links = document.querySelectorAll('h3.r a');
+    var response = {};
+
+    //response.searchEnd = '' + document.querySelector('.pn');
+
+    response.html = document.querySelector('body').innerHTML;
+    //searchEnd = document.querySelectorAll('#pnnext') === null;
+    //this.echo(document.querySelectorAll('#pnnext'));
+    response.data = Array.prototype.map.call(links, function(e) {
+        return e.getAttribute('href');
+    });
+
+    return response;
+}
+
+function evalAndAddLinks(ctx) {
+  var res = ctx.evaluate(getLinks);
+
+  res.data.forEach(function(v) { console.log( v.slice(7).split('&')[0] )});
+}
+
+function checkNextPageAndClick() {
+  evalAndAddLinks(this);
+
+  if (this.getHTML('#nav td.b:last-child > a'))
+    this.thenClick('#nav td.b:last-child > a', checkNextPageAndClick);
+
+  console.log( this.getCurrentUrl() );
+
+  return false;
+}
